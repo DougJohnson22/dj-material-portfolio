8850564707fb20a301c7f13cc0a3c26407b8aa25
Douglas Johnson <trunks22a@gmail.com>
2021-05-18 16:26:50 -0700

    NavTabs setup, react-router removed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
-import Splash from "./pages/Splash/index"
+import NavTabs from "./components/NavTabs/index"
 
 function App() {
   return (
-    <Router>
-      <Route exact path="/">
-        <Splash />
-      </Route>
-    </Router>
+<NavTabs/>     
   );
 }
 
