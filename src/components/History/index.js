3a6b540917eac95c1852857f5d08ffb156d2f224
Douglas Johnson <trunks22a@gmail.com>
2021-07-13 17:31:27 -0700

    Initial History Modal setup

--- src/components/History/index.js
+++ src/components/History/index.js
@@ -3,8 +3,11 @@ import { makeStyles } from '@material-ui/core/styles';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
-// import IconButton from '@material-ui/core/IconButton';
-// import AdjustIcon from '@material-ui/icons/Adjust';
+import IconButton from '@material-ui/core/IconButton';
+import AdjustIcon from '@material-ui/icons/Adjust';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -18,7 +21,6 @@ const useStyles = makeStyles((theme) => ({
     gridList: {
         width: '75%',
         height: '100%',
-        // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
         transform: 'translateZ(0)',
     },
     titleBar: {
@@ -28,6 +30,17 @@ const useStyles = makeStyles((theme) => ({
     },
     icon: {
         color: 'white',
+    },
+    modal: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    paper: {
+        backgroundColor: theme.palette.background.paper,
+        border: '2px solid #000',
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
     },
 }));
 
@@ -65,8 +78,20 @@ const tileData = [
     },
 ];
 
+
+
 export default function HistoryGrid() {
     const classes = useStyles();
+
+    const [open, setOpen] = React.useState(false);
+
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
 
     return (
         <div className={classes.root}>
@@ -77,16 +102,36 @@ export default function HistoryGrid() {
                         <GridListTileBar
                             title={tile.title}
                             titlePosition="top"
-                            // actionIcon={
-                            //     <IconButton aria-label={`open ${tile.title}`} className={classes.icon}>
-                            //         <AdjustIcon />
-                            //     </IconButton>
-                            // }
-                            // actionPosition="left"
+                            actionIcon={
+                                <IconButton aria-label={`open ${tile.title}`} className={classes.icon} onClick={handleOpen}>
+                                    <AdjustIcon />
+                                </IconButton>
+                            }
+                            actionPosition="left"
                             className={classes.titleBar}
                         />
                     </GridListTile>
                 ))}
+
+                <Modal
+                    aria-labelledby="transition-modal-title"
+                    aria-describedby="transition-modal-description"
+                    className={classes.modal}
+                    open={open}
+                    onClose={handleClose}
+                    closeAfterTransition
+                    BackdropComponent={Backdrop}
+                    BackdropProps={{
+                        timeout: 500,
+                    }}
+                >
+                    <Fade in={open}>
+                        <div className={classes.paper}>
+                            <h2 id="transition-modal-title">Transition modal</h2>
+                            <p id="transition-modal-description">react-transition-group animates me.</p>
+                        </div>
+                    </Fade>
+                </Modal>
             </GridList>
         </div>
     );
